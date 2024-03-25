b951f039d458e6458d75fa6de61fa2a8d390a04f
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-03-25 21:21:28 +0100

    feat: greeting

--- src/components/widgets/clock.tsx
+++ src/components/widgets/clock.tsx
@@ -13,8 +13,15 @@ export function Clock() {
     return () => clearInterval(interval);
   }, []);
 
+  let greetingText = 'Jó napot!';
+  const hours = time.getHours();
+  if (hours < 6 || hours > 22) greetingText = 'Jó éjszakát!';
+  else if (hours < 11 && hours >= 6) greetingText = 'Jó reggelt!';
+  else if (hours > 18) greetingText = 'Jó estét!';
+
   return (
     <Widget className='flex flex-col items-center justify-center'>
+      <p className='mb-5 uppercase font-thin'>{greetingText}</p>
       <h2 className='mb-5'>{time.toLocaleTimeString()}</h2>
       <p>{formatHu(time, 'yyyy. MM. dd.')}</p>
       <p>{formatHu(time, 'EEEE')}</p>
